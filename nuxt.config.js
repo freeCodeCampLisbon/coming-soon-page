67e56ca2774e47142688303ca121c8b748180cd0
Pedro Cruz <pedropcruz@outlook.pt>
2020-12-15 02:35:19 +0000

    added netlify toml // add functions to signup to newsletter

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/dotenv',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
