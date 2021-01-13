4e79f5626a6ddc6880d988849d50b133b70b5325
Pedro Cruz <pedropcruz@outlook.pt>
2021-01-13 00:15:03 +0000

    add live page

---
+++ pages/live.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <div class="content is-medium">
+          <h1 class="title is-family-secondary has-text-primary">🔴 LIVE</h1>
+          <figure
+            v-if="article.data.youtube_embedded"
+            class="image is-16by9 mx-0"
+          >
+            <iframe
+              class="has-ratio"
+              loading="lazy"
+              width="640"
+              height="360"
+              :src="$prismic.asLink(article.data.youtube_embedded)"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </figure>
+          <div v-else class="notification is-danger is-light">
+            Please contact the support
+            <a href="mailto:info@freecodecamplisbon.org"
+              >info@freecodecamplisbon.org</a
+            >
+            to check what's the problem. Thanks camper!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $prismic, error }) {
+    try {
+      const article = await $prismic.api.getSingle('meetups', {
+        orderings: '[my.meetups.date_time desc]',
+      })
+
+      return { article }
+    } catch (e) {
+      error({ statusCode: '404' })
+    }
+  },
+  data: () => ({
+    article: [],
+  }),
+  mounted() {
+    console.log(this.article)
+  },
+}
+</script>
